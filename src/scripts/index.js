967cb51e9a7bdcbace3a374ad82b66a060f2a7bc
angelo.p <mrpalaiologos@gmail.com>
2021-04-26 20:30:07 +0200

    in a good way

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,9 +3,9 @@
 //document.write("<h1> Hi h1>");
 
 const addToGamwlist = (e) => {
+  // Preventing the from sending information out aka Reloading the page
   e.preventDefault();
   let ouruserprojectdata = document.querySelector("#ouruserprojectdata").value;
-
   console.log(ouruserprojectdata);
   if (ouruserprojectdata != "") {
     // ftiaxnoume ena li
@@ -14,18 +14,44 @@ const addToGamwlist = (e) => {
     let texti = document.createTextNode(ouruserprojectdata);
     //vazoume me .appendChild() to text tou giouzer mesa sto list mas
     newListitem.appendChild(texti);
-    // Adding random colour to my list ama theloume
-    //newListitem.style.color = "black";
-    //rixnoume to newListitem mesa sto item me to //class name bigresult
+    //vazoume me .appendChild() to list item mas mesa sto bigresult mas
     document.querySelector(".bigresult").appendChild(newListitem);
+    //ftiaxnoume divaki me 2 koubia
+    const newbox = document.createElement("div");
+    const koubidone = document.createElement("button");
+    const koubidelete = document.createElement("button");
+    const newprjectplaceholder = document.createElement("input");
+    koubidone.innerHTML = "ready ✓";
+    koubidelete.innerHTML = "let it go✗";
+    newprjectplaceholder.placeholder = "important notes";
+    newprjectplaceholder.className = "forthenotbook";
+
+    //
+    // Injecting the buttons into the our box
+    newbox.appendChild(koubidone);
+    newbox.appendChild(koubidelete);
+    newbox.appendChild(newprjectplaceholder);
+    newListitem.appendChild(newbox);
+    // Adding class
+
+    koubidone.classList.add("done");
+    // Complete function
+
+    // Complete function
+    // toggle will check if the class name exist, will remove it and if it's not will add it
+    const checkit = () => newListitem.classList.toggle("completed");
+    koubidone.addEventListener("click", checkit);
+    // const del = () => newLi.remove() ;
+    // Adding event listener to my buttons
+    koubidelete.addEventListener("click", () => newListitem.remove());
   } else {
     document.querySelector("#ouruserprojectdata").placeholder =
-      "Please Enter text first";
+      "write here your task...";
   }
 };
+// Adding event listener to the form
+document.querySelector("form").addEventListener("submit", addToGamwlist);
 const addToList = (e) => {
-  // Preventing the from sending information out aka Reloading the page
-  e.preventDefault();
   let userData = document.querySelector("#userData").value;
 
   // Checking my input if it has a text
@@ -34,27 +60,24 @@ const addToList = (e) => {
 
     let text = document.createTextNode(userData);
     newLi.appendChild(text);
-    // Adding random colour to my list item
-    newLi.style.color = colorGen();
+
     document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userData").value = "";
-    // Other way to add random colours for all of the list items that are in my page
-    // const listItems = document.querySelectorAll("li");
-    // listItems.forEach((item) => {
-    //   item.style.color = colorGen();
-    // });
     const newContainer = document.createElement("div");
     const doneButton = document.createElement("button");
     const deleteButton = document.createElement("button");
+
     // const checkBox = document.createElement("input");
     // checkBox.type = "checkbox";
     // newContainer.appendChild(checkBox);
     // Adding some text
     doneButton.innerHTML = "✓";
     deleteButton.innerHTML = "✗";
+
     // Injecting the buttons into the container
     newContainer.appendChild(doneButton);
     newContainer.appendChild(deleteButton);
+
     // Adding class
     doneButton.classList.add("done");
     newLi.appendChild(newContainer);
@@ -69,23 +92,11 @@ const addToList = (e) => {
     document.querySelector("#userData").placeholder = "Please Enter text first";
   }
 };
-// Random colour generator in hexa number
-const colorGen = () => {
-  let result = "#";
-  let colorCode = "0123456789ABCDEF";
-  for (let i = 0; i < 6; i++) {
-    result += colorCode[Math.floor(Math.random() * 16)];
-  }
-  return result;
-};
-// Functional check which key the user is pressing
-const keyCheck = (event) => {
-  console.log(event);
-  if (e.key == "Enter") addToList();
+
+const keyCheck = (e) => {
+  //console.log(event);
+  if (e.key == "Enter") addToGamwlist(e);
 };
 // Adding event listener to the input
 // const userInput = document.querySelector("#userData");
 // userInput.addEventListener("keypress", keyCheck);
-
-// Adding event listener to the form
-document.querySelector("form").addEventListener("submit", addToGamwlist);
