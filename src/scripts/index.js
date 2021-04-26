0a288f6c4993b1da1ce3c6c0ecc6681c46352047
angelo.p <mrpalaiologos@gmail.com>
2021-04-26 11:54:07 +0200

    exploring js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,4 +9,13 @@ const create = () => {
   newElement.appendChild(newText);
   document.body.appendChild(newElement);
 };
-const addToprojectList = () => {};
+const addanewgamwproject = () => {
+  //we grab the id with this queryselector
+  let gamwuserdata = document.querySelector("#gamwuserdata");
+  //we create with document.createElement("create smth"); our new li (it is empty at the first place)
+  newlistitem = document.createElement("li");
+  //we create document.createTextNode(put a text "hi" or a var) the upcoming text that will show up
+  let textfromuser = document.createTextNode(gamwuserdata);
+  //with this ourvar.appendChild(where to) we place our new var ->newlistitem inside the input
+  newlistitem.appendChild(textfromuser);
+};
