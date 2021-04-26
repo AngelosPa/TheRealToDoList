a24f6a8f1b33396fa2e0ec7fef0047342a9d166e
angelo.p <mrpalaiologos@gmail.com>
2021-04-26 12:11:40 +0200

    basic js done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,13 +9,16 @@ const create = () => {
   newElement.appendChild(newText);
   document.body.appendChild(newElement);
 };
-const addanewgamwproject = () => {
-  //we grab the id with this queryselector
-  let gamwuserdata = document.querySelector("#gamwuserdata");
+const addanewourtask = () => {
+  //we grab the value of this input with this querySelector
+  let ouruserdata = document.querySelector("#ouruserdata").value;
   //we create with document.createElement("create smth"); our new li (it is empty at the first place)
-  newlistitem = document.createElement("li");
+  let newlistitem = document.createElement("li");
   //we create document.createTextNode(put a text "hi" or a var) the upcoming text that will show up
-  let textfromuser = document.createTextNode(gamwuserdata);
-  //with this ourvar.appendChild(where to) we place our new var ->newlistitem inside the input
+  let textfromuser = document.createTextNode(ouruserdata);
+  //with this ourNiceBsElement.appendChild(what we place) we place our new textfromuser inside the newlistitem
   newlistitem.appendChild(textfromuser);
+  // now we place our newlistitem inside our ul with the class name result
+  document.querySelector(".result").appendChild(newlistitem);
+  document.querySelector("#ouruserdata").value = "";
 };
