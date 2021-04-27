f63803c9ebe8018e46086ceb8ff299e4fd46ef84
angelo.p <mrpalaiologos@gmail.com>
2021-04-27 11:01:55 +0200

    updated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -51,47 +51,6 @@ const addToGamwlist = (e) => {
 };
 // Adding event listener to the form
 document.querySelector("form").addEventListener("submit", addToGamwlist);
-const addToList = (e) => {
-  let userData = document.querySelector("#userData").value;
-
-  // Checking my input if it has a text
-  if (userData != "") {
-    let newLi = document.createElement("li");
-
-    let text = document.createTextNode(userData);
-    newLi.appendChild(text);
-
-    document.querySelector(".result").appendChild(newLi);
-    document.querySelector("#userData").value = "";
-    const newContainer = document.createElement("div");
-    const doneButton = document.createElement("button");
-    const deleteButton = document.createElement("button");
-
-    // const checkBox = document.createElement("input");
-    // checkBox.type = "checkbox";
-    // newContainer.appendChild(checkBox);
-    // Adding some text
-    doneButton.innerHTML = "✓";
-    deleteButton.innerHTML = "✗";
-
-    // Injecting the buttons into the container
-    newContainer.appendChild(doneButton);
-    newContainer.appendChild(deleteButton);
-
-    // Adding class
-    doneButton.classList.add("done");
-    newLi.appendChild(newContainer);
-    // Complete function
-    // toggle will check if the class name exist, will remove it and if it's not will add it
-    const check = () => newLi.classList.toggle("completed");
-    // const del = () => newLi.remove() ;
-    // Adding event listener to my buttons
-    deleteButton.addEventListener("click", () => newLi.remove());
-    doneButton.addEventListener("click", check);
-  } else {
-    document.querySelector("#userData").placeholder = "Please Enter text first";
-  }
-};
 
 const keyCheck = (e) => {
   //console.log(event);
