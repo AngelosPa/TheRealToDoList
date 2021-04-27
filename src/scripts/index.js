2f3d60d534872909da00e33cede4509c171346c8
angelo.p <mrpalaiologos@gmail.com>
2021-04-27 11:52:46 +0200

    fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,23 +20,12 @@ const addToGamwlist = (e) => {
     const newbox = document.createElement("div");
     const koubidone = document.createElement("button");
     const koubidelete = document.createElement("button");
-    const newprjectplaceholder = document.createElement("input");
+
     koubidone.innerHTML = "ready ✓";
     koubidelete.innerHTML = "let it go✗";
-    newprjectplaceholder.placeholder = "important notes";
-    newprjectplaceholder.className = "forthenotbook";
-
-    //
-    // Injecting the buttons into the our box
-    newbox.appendChild(koubidone);
     newbox.appendChild(koubidelete);
-    newbox.appendChild(newprjectplaceholder);
+    newbox.appendChild(koubidone);
     newListitem.appendChild(newbox);
-    // Adding class
-
-    koubidone.classList.add("done");
-    // Complete function
-
     // Complete function
     // toggle will check if the class name exist, will remove it and if it's not will add it
     const checkit = () => newListitem.classList.toggle("completed");
@@ -51,11 +40,48 @@ const addToGamwlist = (e) => {
 };
 // Adding event listener to the form
 document.querySelector("form").addEventListener("submit", addToGamwlist);
-
 const keyCheck = (e) => {
   //console.log(event);
   if (e.key == "Enter") addToGamwlist(e);
 };
-// Adding event listener to the input
-// const userInput = document.querySelector("#userData");
-// userInput.addEventListener("keypress", keyCheck);
+// const addToList = (e) => {
+//   let userData = document.querySelector("#userData").value;
+
+//   // Checking my input if it has a text
+//   if (userData != "") {
+//     let newLi = document.createElement("li");
+
+//     let text = document.createTextNode(userData);
+//     newLi.appendChild(text);
+
+//     document.querySelector(".result").appendChild(newLi);
+//     document.querySelector("#userData").value = "";
+//     const newContainer = document.createElement("div");
+//     const doneButton = document.createElement("button");
+//     const deleteButton = document.createElement("button");
+
+//     // const checkBox = document.createElement("input");
+//     // checkBox.type = "checkbox";
+//     // newContainer.appendChild(checkBox);
+//     // Adding some text
+//     doneButton.innerHTML = "✓";
+//     deleteButton.innerHTML = "✗";
+
+//     // Injecting the buttons into the container
+//     newContainer.appendChild(doneButton);
+//     newContainer.appendChild(deleteButton);
+
+//     // Adding class
+//     doneButton.classList.add("done");
+//     newLi.appendChild(newContainer);
+//     // Complete function
+//     // toggle will check if the class name exist, will remove it and if it's not will add it
+//     const check = () => newLi.classList.toggle("completed");
+//     // const del = () => newLi.remove() ;
+//     // Adding event listener to my buttons
+//     deleteButton.addEventListener("click", () => newLi.remove());
+//     doneButton.addEventListener("click", check);
+//   } else {
+//     document.querySelector("#userData").placeholder = "Please Enter text first";
+//   }
+// };
